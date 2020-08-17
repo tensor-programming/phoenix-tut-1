9419ebff7bfa50e824a666afeee77bd6394e7e8b
Derek Pappas <derek.pappas@yoterra.com>
2020-08-17 15:31:32 -0700

    adding coboy dep back in

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule Chatter.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:cowboy, "~> 1.0"},
      {:plug_cowboy, "~> 1.0"}]
   end
 
