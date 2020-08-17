9688dd7715606d0f34d4c60061548bb8c235b65d
Derek Pappas <derek.pappas@yoterra.com>
2020-08-17 14:31:07 -0700

    Fixed a problem with the cowboy plug

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Chatter.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:plug_cowboy, "~> 1.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
